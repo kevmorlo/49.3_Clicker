335c9946c1223365d43f817a1f6141fae4f91751
Kévin Lebeau <kevin.lebeau-pro@outlook.fr>
2023-10-20 18:12:59 +0200

    ajout de la page index.php

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
-require './pages/base.php'; // Inclus le modèle base.html
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>49.3 Clicker</title>
+  <link rel="shortcut icon" href="../media/img/49.3.png" type="image/x-icon">
+  <link rel="stylesheet" href="../code/css/style.css">
+  <script src="../code/js/script.js"></script>
+</head>
 <body>
-    <a href=""></a>
-    <div id="affichage">0</div>
+    <nav class="Navbar">
+        <img src="../media/img/49.3_clicker_banniere.png" alt="Logo de 49.3 Clicker" class="Banniere_img">
+        <ul class="Navbar_boutons">
+            <li class="Navbar_liste">
+                <a href="./pages/connexion.php" class="Navbar_bouton" id="Navbar_Bouton_connexion">Se connecter</a>
+            </li>
+        </ul>
+    </nav>
+    <h1 id="Index_h1">49.3 clicker, le meilleur générateur de 49.3 !</h1>
+    <p class="Index_texte">49.3 clicker est un projet d’école développé par un étudiant en informatique dans le but de découvrir le javascript, cette application bien que ressemblant fortement aux sites officiels du gouvernement n’a aucun lien de près ou de loin avec le gouvernement français, et ne vise pas à se faire passer pour celui-ci.</p>
+    <p class="Index_texte">L’objectif principal de cette application est simplement d’effectuer une satire du gouvernement tout en proposant une application intuitive, interactive et amusante.
+Si vous pensez avoir rencontré un site de phishing (un site qui se fait passer pour un autre dans le but de vous soutirer des informations personnelles) signalez-le sur service-public.fr.</p>
+    <form action="./pages/creation.php" class="Index_form">
+        <input type="submit" value="Commencer à jouer" class="Valider_bouton">
+    </form>        
 </body>
